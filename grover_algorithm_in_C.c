c57dcf7c9dffe7240fa42b38fd532f0345a2365f
mnakul <50168621+mnakul@users.noreply.github.com>
2019-05-30 11:11:17 +0000

    Add files via upload

---
+++ grover_algorithm_in_C.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<math.h>
+float average(int n,float * amp){
+  float sum=0.0,mean;
+  for(int i=0;i<n;i++)
+    sum = sum + *(amp+i) ;
+  mean = sum/n;
+  return mean;
+}
+int main(){
+  int n,flag=1;
+  scanf("%d\n",&n);
+  float a[n];float amp[n];float mean,k;                            //array a stores the values
+  for(int i=0;i<n;i++){                                            //array amp stores the corresponding amplitudes
+    amp[i]=1;
+  }
+  for(int i=0;i<n;i++)
+    scanf("%f\n",&a[i]);
+  scanf("%f",&k);
+  for(int i=1;i<=n;i++){
+    for(int j=0;j<n;j++){
+      if(a[j]==k)
+        amp[j]=-amp[j];
+    }
+    mean=fabs(average(n,amp));
+    for(int k=0;k<n;k++){
+       amp[k] = fabs(-amp[k] + 2 * mean);
+    }
+    for(int p=0;p<n;p++)                                           //this for loop prints corresponding amplitude
+      printf("%f\t",amp[p]);                                            
+    printf("\n");
+  }
+  mean = average(n,amp);
+  for(int i=0;i<n;i++)
+  {
+    if(amp[i]>mean){
+      printf("YES");
+      flag=0;
+    }
+  }
+  if(flag)
+  printf("NO");
+}
